d72389831b8d33e57634acdd48d75f33baaa5d07
“shucong.zhang” <728641760shu@sina.com>
2025-04-30 18:41:09 +0800

    add process fun

---
+++ cmake-build-debug/CMakeFiles/IPAY_autogen.dir/cmake_clean.cmake
@@ -0,0 +1,21 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/IPAY_autogen"
+  "IPAY_autogen/include/View/ui_advancedfunction.h"
+  "IPAY_autogen/include/View/ui_cashregisterkeyboard.h"
+  "IPAY_autogen/include/View/ui_cashregistersetting.h"
+  "IPAY_autogen/include/View/ui_commonsetting.h"
+  "IPAY_autogen/include/View/ui_controlscreen.h"
+  "IPAY_autogen/include/View/ui_miscellaneousfunction.h"
+  "IPAY_autogen/include/View/ui_orderdetails.h"
+  "IPAY_autogen/include/View/ui_paymentplatform.h"
+  "IPAY_autogen/include/View/ui_playsetting.h"
+  "IPAY_autogen/include/View/ui_remindersetting.h"
+  "IPAY_autogen/include/View/ui_scancodesetting.h"
+  "IPAY_autogen/mocs_compilation.cpp"
+  "IPAY_autogen/timestamp"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/IPAY_autogen.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/IPAY_autogen.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty custom commands generated dependencies file for IPAY_autogen.
+# This may be replaced when dependencies are built.
