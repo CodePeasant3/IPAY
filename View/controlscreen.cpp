e3046fea70d9b0693ebaacc059657f379f8ee535
Haoming <juhaoming@outlook.com>
2025-05-13 21:56:12 +0800

    修正截图确定/取消按钮的位置偏移并使更加明显

--- View/controlscreen.cpp
+++ View/controlscreen.cpp
@@ -17,6 +17,13 @@ void ControlScreen::ScreenInit()
 {
     connect(ui->cancelBtn, SIGNAL(clicked()), this, SLOT(cancelBtn_slot()));
     connect(ui->finishBtn, SIGNAL(clicked()), this, SLOT(finishBtn_slot()));
+    this->setStyleSheet(
+        "QWidget {"
+        "    border: 2px solid red;"  // 设置2像素宽的红色实线边框
+        "    border-radius: 5px;"     // 设置边框圆角
+        "    background-color: #f0f0f0;"  // 设置背景颜色以便于观察
+        "}"
+        );
 }
 
 void ControlScreen::cancelBtn_slot()
